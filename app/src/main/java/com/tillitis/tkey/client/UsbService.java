e540eca0ca8d89fa97c021d68610bfa54d065f77
iknek <imad.alihodzic@yahoo.com>
2023-08-18 15:37:29 +0200

    Configured this library to connect and send/recieve data from the TKey (unreliably).

--- app/src/main/java/com/tillitis/tkey/client/UsbService.java
+++ app/src/main/java/com/tillitis/tkey/client/UsbService.java
@@ -1,70 +1,179 @@
+/*
+ * Copyright (C) 2022, 2023 - Tillitis AB
+ * SPDX-License-Identifier: GPL-2.0-only
+*/
 package com.tillitis.tkey.client;
 
-import android.app.Activity;
-import android.content.Context;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbManager;
-
-import com.felhr.usbserial.UsbSerialDevice;
-import com.felhr.usbserial.UsbSerialInterface;
-
-import java.util.HashMap;
-
-public class UsbService {
+import static android.hardware.usb.UsbManager.EXTRA_PERMISSION_GRANTED;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.*;
+import android.hardware.usb.*;
+import android.os.*;
+import com.felhr.usbserial.*;
+import java.util.*;
+public class UsbService extends Service {
+    public static final String prefix = "android.hardware.usb.action";
+    public static final String ACTION_USB_READY = prefix + "USB_READY";
+    public static final String ACTION_USB_ATTACHED = prefix + "USB_DEVICE_ATTACHED";
+    public static final String ACTION_USB_DETACHED = prefix + "USB_DEVICE_DETACHED";
+    public static final String ACTION_USB_NOT_SUPPORTED = prefix + "USB_NOT_SUPPORTED";
+    public static final String ACTION_NO_USB = prefix + "NO_USB";
+    private static final String appPrefix = "com.tillitis.tkey.client";
+    public static final String ACTION_USB_PERMISSION_GRANTED =  appPrefix + "usbservice.USB_PERMISSION_GRANTED";
+    public static final String ACTION_USB_PERMISSION_NOT_GRANTED = appPrefix + "usbservice.USB_PERMISSION_NOT_GRANTED";
+    public static final String ACTION_USB_DISCONNECTED = appPrefix + "usbservice.USB_DISCONNECTED";
+    public static final String ACTION_CDC_DRIVER_NOT_WORKING = appPrefix + "connectivityservices.ACTION_CDC_DRIVER_NOT_WORKING";
+    public static final String ACTION_USB_DEVICE_NOT_WORKING = appPrefix + "connectivityservices.ACTION_USB_DEVICE_NOT_WORKING";
+    private static final String ACTION_USB_PERMISSION = "com.tillitis.tkey.USB_PERMISSION";
+    public static boolean SERVICE_CONNECTED = false;
+    private final IBinder binder = new UsbBinder();
+    private Context context;
     private UsbManager usbManager;
     private UsbDevice device;
     private UsbDeviceConnection connection;
-    private UsbSerialDevice serialDevice;
-    private static final int BAUD_RATE = 62500;
+    private UsbSerialDevice serialPort;
+    private boolean serialPortConnected;
 
-    public UsbService(Activity activity) {
-        usbManager = (UsbManager) activity.getSystemService(Context.USB_SERVICE);
+    private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context arg0, Intent arg1) {
+            if (arg1.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted = arg1.getBooleanExtra(EXTRA_PERMISSION_GRANTED,true);
+                if (granted) // User accepted our USB connection. Try to open the device as a serial port
+                {
+                    Intent intent = new Intent(ACTION_USB_PERMISSION_GRANTED);
+                    arg0.sendBroadcast(intent);
+                    connection = usbManager.openDevice(device);
+                    new ConnectionThread().start();
+                } else
+                {
+                    Intent intent = new Intent(ACTION_USB_PERMISSION_NOT_GRANTED);
+                    arg0.sendBroadcast(intent);
+                }
+            } else if (arg1.getAction().equals(ACTION_USB_ATTACHED)) {
+                if (!serialPortConnected)
+                    findSerialPortDevice(); // A USB device has been attached. Try to open it as a Serial port
+            } else if (arg1.getAction().equals(ACTION_USB_DETACHED)) {
+                // Usb device was disconnected. send an intent to the Main Activity
+                Intent intent = new Intent(ACTION_USB_DISCONNECTED);
+                arg0.sendBroadcast(intent);
+                if (serialPortConnected) {
+                    serialPort.syncClose();
+                }
+                serialPortConnected = false;
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        this.context = this;
+        serialPortConnected = false;
+        UsbService.SERVICE_CONNECTED = true;
+        setFilter();
+        usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        findSerialPortDevice();
     }
 
-    //Device seems to connect properly. No issues with method.
-    public boolean connectDevice() {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        serialPort.close();
+        unregisterReceiver(usbReceiver);
+        UsbService.SERVICE_CONNECTED = false;
+    }
+
+    private void findSerialPortDevice() {
         HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
         if (!usbDevices.isEmpty()) {
-            for (UsbDevice device : usbDevices.values()) {
-                this.device = device;
-                connection = usbManager.openDevice(device);
-                serialDevice = UsbSerialDevice.createUsbSerialDevice(device, connection);
-                if (serialDevice != null) {
-                    if (serialDevice.syncOpen()) {
-                        serialDevice.setBaudRate(BAUD_RATE);
-                        serialDevice.setDataBits(UsbSerialInterface.DATA_BITS_8);
-                        serialDevice.setStopBits(UsbSerialInterface.STOP_BITS_1);
-                        serialDevice.setParity(UsbSerialInterface.PARITY_NONE);
-                        serialDevice.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                        return true;
-                    } else {
-                        return false;
-                    }
+            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+                device = entry.getValue();
+                if (UsbSerialDevice.isSupported(device)) {
+                    requestUserPermission();
+                    break;
                 } else {
-                    return false;
+                    connection = null;
+                    device = null;
                 }
             }
+            if (device == null) {
+                Intent intent = new Intent(ACTION_NO_USB);
+                sendBroadcast(intent);
+            }
+        } else {
+            Intent intent = new Intent(ACTION_NO_USB);
+            sendBroadcast(intent);
         }
-        return false;
     }
 
-    //Might somewhat work, as TKey blinks red when attempting to load an app.
+    private void setFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(ACTION_USB_DETACHED);
+        filter.addAction(ACTION_USB_ATTACHED);
+        registerReceiver(usbReceiver, filter);
+    }
+
+    private void requestUserPermission() {
+        PendingIntent mPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
+        usbManager.requestPermission(device, mPendingIntent);
+    }
+
+    public class UsbBinder extends Binder {
+        public UsbService getService() {
+            return UsbService.this;
+        }
+    }
+
     public void writeData(byte[] data) {
-        if (serialDevice != null && data != null) {
-            serialDevice.syncWrite(data,0);
-        }
+        if (serialPort != null)
+            serialPort.syncWrite(data, 100);
     }
 
-    //Doesn't work, returns 0.
-    public byte[] readData(int length) {
-        if (serialDevice != null) {
-            byte[] buffer = new byte[length];
-            int bytesRead = serialDevice.syncRead(buffer, 0);
-            if (bytesRead > 0) {
-                return buffer;
+    public byte[] readData(int size) {
+        byte[] buffer = new byte[size];
+        serialPort.syncRead(buffer, 20);
+        return buffer;
+    }
+
+    private class ConnectionThread extends Thread {
+        @Override
+        public void run() {
+            serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+            if (serialPort != null) {
+                if (serialPort.syncOpen()) {
+                    serialPortConnected = true;
+                    serialPort.setBaudRate(62500);
+                    serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                    serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                    serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                    serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                    Intent intent = new Intent(ACTION_USB_READY);
+                    context.sendBroadcast(intent);
+                } else {
+                    Intent intent;
+                    if (serialPort instanceof CDCSerialDevice) {
+                        intent = new Intent(ACTION_CDC_DRIVER_NOT_WORKING);
+                    } else {
+                        intent = new Intent(ACTION_USB_DEVICE_NOT_WORKING);
+                    }
+                    context.sendBroadcast(intent);
+                }
+            } else {
+                Intent intent = new Intent(ACTION_USB_NOT_SUPPORTED);
+                context.sendBroadcast(intent);
             }
         }
-        return null;
     }
 }
